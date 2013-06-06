41bc7b4ba5e449304a477cfa9567cac066996db6
AquarHEAD L <aquarhead@gmail.com>
2013-06-06 20:11:54 +0800

    Initial Commit

---
+++ CMinX/CMXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMXAppDelegate.h
+//  CMinX
+//
+//  Created by AquarHEAD L. on 6/6/13.
+//  Copyright (c) 2013 Team.TeaWhen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CMXAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CMinX/CMXAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CMXAppDelegate.m
+//  CMinX
+//
+//  Created by AquarHEAD L. on 6/6/13.
+//  Copyright (c) 2013 Team.TeaWhen. All rights reserved.
+//
+
+#import "CMXAppDelegate.h"
+
+@implementation CMXAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
